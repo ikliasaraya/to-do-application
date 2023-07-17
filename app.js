63f97feaa2a56203bd65fdbde1c420732240a5f9
Ali İlker Sarıkaya <65564309+ikliasaraya@users.noreply.github.com>
2023-07-17 15:14:42 +0300

    View oluşturulması.

---
+++ app.js
@@ -0,0 +1,16 @@
+//  Buttons
+
+const addBtn = document.getElementById("button-add");
+const deleteAllBtn = document.getElementById("button-clear-list");
+const searchBtn = document.getElementById("button-search");
+const deleteBtn = document.getElementsByClassName('button-delete'); // returns html collection
+const checkBtn = document.querySelectorAll('.button-check'); // returns node list
+
+// Inputs
+
+// const addInput;
+// const searchInput;
+
+
+console.log(deleteBtn);
+console.log(checkBtn);
